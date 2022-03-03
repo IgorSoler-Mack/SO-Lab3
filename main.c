5b0a536750fa7fea3ae068ae16cb6e7f1f5d9718
IgorSoler-Mack <99455511+IgorSoler-Mack@users.noreply.github.com>
2022-03-03 19:00:51 -0300

    Adição de Comentários

--- main.c
+++ main.c
@@ -6,16 +6,18 @@ int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
 	int i, n;
 	
-	if (argc != 2){ 
-		fprintf(stderr, "Usage: %s processes\n", argv[0]);
-		return 1;
+	if (argc != 2){ // Garante que a quantidade de parametros seja 2
+		fprintf(stderr, "Usage: %s processes\n", argv[0]); // Imprime erro para sys.stderr caso a quantidade de args seja incorreta
+		return 1; // Retorna Falha
 	}
-	n = atoi(argv[1]); 
-	for (i = 1; i < n; i++)
+	
+	n = atoi(argv[1]); // String para Int
+	
+	for (i = 1; i < n; i++) // Gera i processos filho
 		if (childpid = fork()){ 
 			break;
 		}
-	fprintf(stdout, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+	fprintf(stdout, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", // Imprime ID de Processo, Pai e Filho
 			i, (long)getpid(), (long)getppid(), (long)childpid);
 	return 0;
 }
